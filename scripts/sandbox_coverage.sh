120ebd06d497bf33f860444d8369b08fcb78bc42
Salvatore Mesoraca <s.mesoraca16@gmail.com>
2026-08-22 18:46:16 +0200

    Enforce 100% coverage for everything
    Signed-off-by: Salvatore Mesoraca <s.mesoraca16@gmail.com>

--- scripts/sandbox_coverage.sh
+++ scripts/sandbox_coverage.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../"
 
 rm -f .coverage
 pytest --cov-fail-under=0 tests/test_integration.py
-python3 -m coverage report --include="*/src/tiz/sandbox_worker.py" --show-missing --fail-under=89
+python3 -m coverage report --include="*/src/tiz/sandbox_worker.py" --show-missing --fail-under=100
--- scripts/web_api_coverage.sh
+++ scripts/web_api_coverage.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/../"
 
 rm -f .coverage
 pytest --cov-fail-under=0 tests/test_web_api_integration.py
-python3 -m coverage report --include="*/src/tiz/web_api.py" --show-missing --fail-under=89
+python3 -m coverage report --include="*/src/tiz/web_api.py" --show-missing --fail-under=100
